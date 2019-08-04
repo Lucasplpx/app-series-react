7138eeb4c06957fec7c67ab814217703d6abf54e
Lucas Passos <lucasplpx@gmail.com>
2019-08-04 16:46:08 -0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Header from './Header'
 import Home from './Home'
@@ -20,10 +20,12 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Route path='/' exact component={Home} />      
-        <Route path='/generos/novo' exact component={NovoGenero} />
-        <Route path='/generos' exact component={Generos} />
-        <Route path='/generos/:id' exact component={EditarGenero} />
+        <Switch>
+          <Route path='/' exact component={Home} />      
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
+        </Switch>
         <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
